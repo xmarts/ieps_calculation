f1e9b3b1eb9fddd365809ceca4510772df5734ed
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-06-26 10:22:54 -0500

    change if

--- ieps_calculation/models/models.py
+++ ieps_calculation/models/models.py
@@ -58,7 +58,7 @@ class SaleOrderLine(models.Model):
 				# If company_id is set, always filter taxes by the company
 				taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
 				line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
-			if line.order_id.partner_id.show_ieps == False:
+			else:
 				fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
 				# If company_id is set, always filter taxes by the company
 				taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
