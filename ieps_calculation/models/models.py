0dd11a72255124c9a8dedd2ae68894bc1e260093
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-06-26 10:39:43 -0500

    revert changes

--- ieps_calculation/models/models.py
+++ ieps_calculation/models/models.py
@@ -53,12 +53,12 @@ class SaleOrderLine(models.Model):
 	@api.depends('tax_id')
 	def _compute_tax_id(self):
 		for line in self:
-			if line.partner_id.show_ieps == True:
+			if line.order_id.partner_id.show_ieps == True:
 				fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
 				# If company_id is set, always filter taxes by the company
 				taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
 				line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
-			else:
+			if line.order_id.partner_id.show_ieps == False:
 				fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
 				# If company_id is set, always filter taxes by the company
 				taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
