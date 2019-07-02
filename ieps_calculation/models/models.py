e3b312de08193f2fe55aa4784ba6d95c854d5b8a
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-07-02 17:39:33 -0500

    fix error

--- ieps_calculation/models/models.py
+++ ieps_calculation/models/models.py
@@ -12,7 +12,7 @@ from werkzeug.urls import url_encode
 
 from odoo import api, exceptions, fields, models, _
 from odoo.tools import email_re, email_split, email_escape_char, float_is_zero, float_compare, \
-    pycompat, date_utils
+	pycompat, date_utils
 from odoo.tools.misc import formatLang
 
 from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
@@ -451,38 +451,6 @@ class AccountInvoice(models.Model):
 
 	@api.multi
 	def get_taxes_values(self):
-		if self.type not in ('out_invoice', 'out_refund'):
-			res = []
-			for line in self.invoice_line_ids:
-				if not line.account_id:
-					continue
-				if line.quantity==0:
-					continue
-				tax_ids = []
-				for tax in line.invoice_line_tax_ids:
-					tax_ids.append((4, tax.id, None))
-					for child in tax.children_tax_ids:
-						if child.type_tax_use != 'none':
-							tax_ids.append((4, child.id, None))
-				analytic_tag_ids = [(4, analytic_tag.id, None) for analytic_tag in line.analytic_tag_ids]
-
-				move_line_dict = {
-					'invl_id': line.id,
-					'type': 'src',
-					'name': line.name,
-					'price_unit': line.price_unit,
-					'quantity': line.quantity,
-					'price': line.price_subtotal,
-					'account_id': line.account_id.id,
-					'product_id': line.product_id.id,
-					'uom_id': line.uom_id.id,
-					'account_analytic_id': line.account_analytic_id.id,
-					'analytic_tag_ids': analytic_tag_ids,
-					'tax_ids': tax_ids,
-					'invoice_id': self.id,
-				}
-				res.append(move_line_dict)
-			return res
 		if self.type in ('out_invoice', 'out_refund'): 
 			tax_grouped = {}
 			round_curr = self.currency_id.round
@@ -532,6 +500,25 @@ class AccountInvoice(models.Model):
 						else:
 							tax_grouped[key]['amount'] += val['amount']
 							tax_grouped[key]['base'] += round_curr(val['base'])
+			return tax_grouped
+		else:
+			tax_grouped = {}
+			round_curr = self.currency_id.round
+			for line in self.invoice_line_ids:
+				if not line.account_id:
+					continue
+				price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+				taxes = line.invoice_line_tax_ids.compute_all(price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id)['taxes']
+				for tax in taxes:
+					val = self._prepare_tax_line_vals(line, tax)
+					key = self.env['account.tax'].browse(tax['id']).get_grouping_key(val)
+
+					if key not in tax_grouped:
+						tax_grouped[key] = val
+						tax_grouped[key]['base'] = round_curr(val['base'])
+					else:
+						tax_grouped[key]['amount'] += val['amount']
+						tax_grouped[key]['base'] += round_curr(val['base'])
 			return tax_grouped
 
 class AccountMoveLine(models.Model):
