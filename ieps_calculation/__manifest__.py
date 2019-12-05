424d6d4fabe9ebedd36edb25e342694ab92ec224
Pablo Osorio <pablo.osorio@xmarts.com>
2019-12-05 13:54:44 -0600

    fis ieps on invoice report

--- ieps_calculation/__manifest__.py
+++ ieps_calculation/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ieps_calculation",
+    'name': "Calculo de IEPS",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Calculo de IEPS en Facturacion y Ventas""",
 
     'description': """
-        Long description of module's purpose
+        Reconfiguracion para calculo de IEPS
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xmarts",
+    'website': "https://www.xmarts.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'account',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -27,6 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        #'reports/report_ieps.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
