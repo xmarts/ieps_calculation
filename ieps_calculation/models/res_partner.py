1144ab5baec453dbb957f560b73513e59575d8fa
victor manuel alonso soto <victor.alonso.xmarts@gmail.com>
2022-03-01 12:53:35 -0600

    [FIX] ieps calculation

---
+++ ieps_calculation/models/res_partner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from collections import OrderedDict
+import json
+import re
+import uuid
+from functools import partial
+
+from lxml import etree
+from dateutil.relativedelta import relativedelta
+from werkzeug.urls import url_encode
+
+from odoo import api, exceptions, fields, models, _
+from odoo.tools import email_re, email_split, email_escape_char, float_is_zero, float_compare, \
+    pycompat, date_utils
+from odoo.tools.misc import formatLang
+
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+
+from odoo.addons import decimal_precision as dp
+import logging
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+    show_ieps = fields.Boolean(string="Mostrar IEPS.", default=False)
