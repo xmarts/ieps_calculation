6b5e9e7d3e294f072362326a9ee08ff359d787a7
victor manuel alonso soto <victor.alonso.xmarts@gmail.com>
2022-09-20 15:40:14 -0500

    [FIX] calculation ieps

--- ieps_calculation/models/inherit_account_move_line.py
+++ ieps_calculation/models/inherit_account_move_line.py
@@ -58,10 +58,12 @@ class AccountMoveLine(models.Model):
 
         print("_prepare_edi_vals_to_export",self.move_id.partner_id.show_ieps)
 
-        if self.move_id.partner_id.show_ieps == True:
-
-            
-
+        is_ieps = 0
+        for s in self.tax_ids:
+            if s.ieps == True:
+                is_ieps += 1
+        print("is_ieps",is_ieps)
+        if is_ieps == 0:
             if self.discount == 100.0:
                 gross_price_subtotal = self.currency_id.round(self.price_unit * self.quantity)
             else:
@@ -79,30 +81,53 @@ class AccountMoveLine(models.Model):
                 'unece_uom_code': self.product_id.product_tmpl_id.uom_id._get_unece_code(),
             }
             return res
-
-
-            
         else:
-            sub_total = 0
-            for s in self.tax_ids:
-                if s.ieps == False:
-                    sub_total += self.currency_id.round((self.price_unit * self.quantity) / ((s.amount / 100) + 1))
 
-            if self.discount == 100.0:
-                gross_price_subtotal = self.currency_id.round(self.price_unit * self.quantity)
-            else:                    
+            if self.move_id.partner_id.show_ieps == True:
 
-                gross_price_subtotal = self.currency_id.round(sub_total / (1 - self.discount / 100.0))
+                
 
-            res = {
-                'line': self,
-                'price_unit_after_discount': self.currency_id.round(self.price_unit * (1 - (self.discount / 100.0))),
-                'price_subtotal_before_discount': gross_price_subtotal,
-                'price_subtotal_unit': self.currency_id.round(sub_total / self.quantity) if self.quantity else 0.0,
-                'price_total_unit': self.currency_id.round(sub_total / self.quantity) if self.quantity else 0.0,
-                'price_discount': gross_price_subtotal - sub_total,
-                'price_discount_unit': (gross_price_subtotal - sub_total) / self.quantity if self.quantity else 0.0,
-                'gross_price_total_unit': self.currency_id.round(gross_price_subtotal / self.quantity) if self.quantity else 0.0,
-                'unece_uom_code': self.product_id.product_tmpl_id.uom_id._get_unece_code(),
-            }
-            return res
+                if self.discount == 100.0:
+                    gross_price_subtotal = self.currency_id.round(self.price_unit * self.quantity)
+                else:
+                    gross_price_subtotal = self.currency_id.round(self.price_subtotal / (1 - self.discount / 100.0))
+
+                res = {
+                    'line': self,
+                    'price_unit_after_discount': self.currency_id.round(self.price_unit * (1 - (self.discount / 100.0))),
+                    'price_subtotal_before_discount': gross_price_subtotal,
+                    'price_subtotal_unit': self.currency_id.round(self.price_subtotal / self.quantity) if self.quantity else 0.0,
+                    'price_total_unit': self.currency_id.round(self.price_total / self.quantity) if self.quantity else 0.0,
+                    'price_discount': gross_price_subtotal - self.price_subtotal,
+                    'price_discount_unit': (gross_price_subtotal - self.price_subtotal) / self.quantity if self.quantity else 0.0,
+                    'gross_price_total_unit': self.currency_id.round(gross_price_subtotal / self.quantity) if self.quantity else 0.0,
+                    'unece_uom_code': self.product_id.product_tmpl_id.uom_id._get_unece_code(),
+                }
+                return res
+
+
+                
+            else:
+                sub_total = 0
+                for s in self.tax_ids:
+                    if s.ieps == False:
+                        sub_total += self.currency_id.round((self.price_unit * self.quantity) / ((s.amount / 100) + 1))
+
+                if self.discount == 100.0:
+                    gross_price_subtotal = self.currency_id.round(self.price_unit * self.quantity)
+                else:                    
+
+                    gross_price_subtotal = self.currency_id.round(sub_total / (1 - self.discount / 100.0))
+
+                res = {
+                    'line': self,
+                    'price_unit_after_discount': self.currency_id.round(self.price_unit * (1 - (self.discount / 100.0))),
+                    'price_subtotal_before_discount': gross_price_subtotal,
+                    'price_subtotal_unit': self.currency_id.round(sub_total / self.quantity) if self.quantity else 0.0,
+                    'price_total_unit': self.currency_id.round(sub_total / self.quantity) if self.quantity else 0.0,
+                    'price_discount': gross_price_subtotal - sub_total,
+                    'price_discount_unit': (gross_price_subtotal - sub_total) / self.quantity if self.quantity else 0.0,
+                    'gross_price_total_unit': self.currency_id.round(gross_price_subtotal / self.quantity) if self.quantity else 0.0,
+                    'unece_uom_code': self.product_id.product_tmpl_id.uom_id._get_unece_code(),
+                }
+                return res
